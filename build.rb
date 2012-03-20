7a7afdd0badefb8df8233bdd7ac8825a32740236
Trond Kjetil Bremnes <tkbremnes@gmail.com>
2012-03-20 23:45:10 +0100

    Initiell versjon av build-skriptet mitt. Legger og til alle definisjoner til i et eget delkapittel sist. Fungerer dog ennå dårlig med litt mer fancye definisjoner. Bilder fungerer ikke.

--- build.rb
+++ build.rb
@@ -1,2 +1,31 @@
-# TODO
-puts "hello world";
+preamble = "% Kompendie \n% @kartoffelmos"
+output = preamble
+defList = "\n\n\#Definisjoner\n\n"
+
+fileList = %x[find . -name '*.md']
+
+fileList.each do |filename|
+	fname = filename.chomp!
+
+	file = File.new(fname, "r")
+	prevLine = ""
+	file.each do |line|
+		if line[0] == 58
+			defList += prevLine + line + "\n\n"
+		end
+		prevLine = line
+	end
+
+	file = File.new(fname, "r")
+	output += "\n\n"
+	output += file.read
+end
+
+output += defList
+
+f = File.new("output.md", "w")
+f.write(output)
+f.close
+
+%x[pandoc output.md -o Kompendie.epub]
+%x[rm output.md]
